431ef14ebed0b19ef67a2175f278f85352df0dbe
ParthaSarathi <ParthaSarathi@DESKTOP-RF1HAOJ>
2019-02-22 04:32:28 +0530

    Minor fixes

--- src/main/java/info/wisdomin/spingwithswagger/Swaggerconfig.java
+++ src/main/java/info/wisdomin/spingwithswagger/Swaggerconfig.java
@@ -7,20 +7,20 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spring.web.plugins.Docket;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
- 
+
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
-
 @EnableSwagger2
 public class Swaggerconfig {
 
-	 @Bean
-	    public Docket postsApi() {
-	        return new Docket(DocumentationType.SWAGGER_2).apiInfo(metadata()).select().paths(regex("/employee.*")).build();
-	    }
-	 
-	    private ApiInfo metadata() {
-	        return new ApiInfoBuilder().title("www.Wisdomin.info").description("Develoopers Guide- Spring API reference").termsOfServiceUrl("https://www.Wisdomin.info/").version("1.0").build();  
-	    }
+	@Bean
+	public Docket postsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(metadata()).select().paths(regex("/employee.*")).build();
+	}
+
+	private ApiInfo metadata() {
+		return new ApiInfoBuilder().title("www.Wisdomin.info").description("Develoopers Guide- Spring API reference")
+				.termsOfServiceUrl("https://www.Wisdomin.info/").version("1.0").build();
+	}
 }
